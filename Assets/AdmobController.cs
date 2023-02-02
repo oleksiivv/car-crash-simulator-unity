53f02fdeab369c274aa1d8d9a0e7060a9e7e7a48
unknown <oleksiypetrochenko@gmail.com>
2023-02-02 22:53:42 +0200

    ios admob setup

--- Assets/AdmobController.cs
+++ Assets/AdmobController.cs
@@ -10,14 +10,21 @@ public class AdmobController : MonoBehaviour
     private InterstitialAd intersitional;
     private BannerView banner;
 
+#if UNITY_IOS
+    private string appId="ca-app-pub-4962234576866611~2264672855";
+    private string intersitionalId="ca-app-pub-4962234576866611/4316121128";
+
+    private string bannerId="ca-app-pub-4962234576866611/9568447809";
+    private string unityAds = "4919218";
+#else
     private string appId="ca-app-pub-4962234576866611~8839623138";
     private string intersitionalId="ca-app-pub-4962234576866611/9838415002";
 
     private string bannerId="ca-app-pub-4962234576866611/8648051445";
+    private string unityAds = "4919219";
+#endif
 
     public static int adsCnt = 1;
-
-    private string unityAds = "4919219";
     
     void Start(){
 
